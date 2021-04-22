b1fa9d430a7ed2698315f9b3152c6e5a4f9a56f2
matipohl <matipohl@gmail.com>
2021-04-22 18:46:28 -0400

    calc

--- Javascript/Calculadora/prueba.js
+++ Javascript/Calculadora/prueba.js
@@ -1,11 +1,27 @@
 var str = "";
 
 function crearString(clickedId) {
+    var patt = /[+*\/-]{1}/gm;
     var newElement = document.getElementById(clickedId).id;
-    var operator = false;
-
-    str += newElement;
+    //Validador
+    if (patt.test(str) == true && patt.test(newElement) == true) {
+        str = str.substring(0, str.length - 1);
+        str += newElement;
+    } else {
+        str += newElement;
+    }
     document.getElementById("txtNumeros").value = str;
+}
+
+function validator(str) {
+    var patt = /[+*\/-]{2}/gm;
+    if (patt.test(str) == true) {
+        console.log("encontró 2 ++");
+        str = str.substring(0, str.length - 1);
+        return str;
+    } else {
+        return str;
+    }
 }
 
 
