083667295577e906b35f60fd9b7ea669867e1085
matipohl <matipohl@gmail.com>
2021-04-24 02:22:41 -0400

    pacman

---
+++ Javascript/Juegos/Pacman/pacman.js
@@ -0,0 +1,36 @@
+var world = [
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    [2, 1, 1, 2, 1, 1, 1, 1, 1, 2],
+    [2, 1, 1, 2, 2, 2, 2, 1, 1, 2],
+    [2, 1, 1, 1, 1, 0, 2, 1, 1, 2],
+    [2, 1, 1, 1, 1, 0, 2, 1, 1, 2],
+    [2, 1, 1, 1, 1, 0, 2, 1, 1, 2],
+    [2, 1, 1, 1, 2, 2, 2, 1, 1, 2],
+    [2, 1, 1, 1, 0, 1, 1, 1, 1, 2],
+    [2, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+]
+
+function drawWorld() {
+    var output = '';
+    for (var i = 0; i < world.length; i++) {
+        output += '<div class="row">';
+        for (var j = 0; j < world[i].length; j++) {
+            if (world[i][j] == 2) {
+                output += '<div class="brick"></div>';
+            } else if (world[i][j] == 1) {
+                output += '<div class="coin"></div>';
+            } else if (world[i][j] == 0) {
+                output += '<div class="empty"></div>';
+            }
+        }
+        output += '</div>';
+    }
+    console.log(output);
+    document.getElementById("world").innerHTML = output;
+}
+
+
+
+//loading
+drawWorld();
