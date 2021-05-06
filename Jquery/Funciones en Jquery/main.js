4702a5a761850a999436eea9103d29a1913d02df
matipohl <matipohl@gmail.com>
2021-05-05 21:51:51 -0400

    funciones en jquery

---
+++ Jquery/Funciones en Jquery/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    //Addclass
+    $('button.add-class').click(function() {
+        $("p").css("color", "red");
+    });
+
+    //ToggleImage
+    $('button.slide-toggle').click(function() {
+        $('#rick').toggleClass("d-none");
+    })
+
+    //Append 
+    $('button.append').click(function() {
+        $("p#append").append(" Este es un nuevo párrafo. ");
+    })
+});
