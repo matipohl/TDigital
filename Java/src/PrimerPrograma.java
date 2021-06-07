ef1c35d3c5de0d888b5dc84228dd81f107be5938
matipohl <matipohl@gmail.com>
2021-06-07 19:32:33 -0400

    primerprograma

---
+++ Java/src/PrimerPrograma.java
@@ -0,0 +1,7 @@
+public class PrimerPrograma {
+	public static void main(String[] args) {
+		System.out.println("Mi nombre es Coding Dojo");
+		System.out.println("Tengo 100 años de edad");
+		System.out.println("Mi ciudad es Burbank, CA");
+	}
+}
