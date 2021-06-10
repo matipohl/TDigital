7dd832f92d0eaa8c5b9d40d6d2486956c895f7c5
matipohl <matipohl@gmail.com>
2021-06-10 10:20:16 -0400

    musica

---
+++ Java/src/MapaHashmatique.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+import java.util.Set;
+
+
+public class MapaHashmatique {
+	HashMap<String,String> track = new HashMap<>();
+
+	public static void main(String[] args) {
+		MapaHashmatique mh = new MapaHashmatique();
+
+		mh.addSong("makarena", "Dale a tu cuerpo alegría makarena");
+		mh.addSong("mayonesa", "Mayoooonesa, ella asdadsadas como haciendo mayonesa");
+		mh.addSong("Rompe", "Rompe rompe rompe asdasda rompe rompe rompe");
+		mh.addSong("Perritos", "Yo tenía 10 perritos, yo tenía 10 perritos...");
+
+		System.out.println(mh.getSong("makarena"));
+
+		mh.printSongs();
+	}
+
+	HashMap<String,String> addSong(String title, String lyrics){
+		track.put(title,lyrics);
+		return track;
+	}
+
+	String getSong(String title){
+		return track.get(title);
+	}
+
+	void printSongs(){
+		Set<String> keys = track.keySet();
+
+		for(String key : keys){
+			System.out.println(key +": " +  track.get(key));
+		}
+	}
+}
